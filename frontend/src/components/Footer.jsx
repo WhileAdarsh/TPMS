b736592dc8b22f846966b35b7d2124a51a96ef79
Codedesigner0 <continue9009@gmail.com>
2025-06-29 22:08:18 +0530

    upgraded

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer({ isSidebarVisible }) {
               target='_blanck'
               className='cursor-pointer font-bold text-blue-500 no-underline hover:text-blue-700'
             >
-              Us 
+              NIT JSR 
             </a>
           </span>
         </div>
